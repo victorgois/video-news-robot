cb4fe11fa179bf7ab7cc0a9808a9e17bb45cbbde
Victor Góis Pacheco <victorgois18@gmail.com>
2020-02-05 14:42:57 -0300

    Delete validate_json.py

--- validate_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-def main ():
-    
-    f= open("new_output","w+")
-    prog = 'python -m json.tool < output.json' >> 'new_output.json'
-    return prog
-    f.write(prog)
-    f.close
